157caba8091b27d334b3de056b10d0787a09ae58
Anteloppy <114159655+Anteloppy@users.noreply.github.com>
2024-04-28 09:24:56 +0700

    0.2

---
+++ Chess Logic/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace Chess_Logic
+{
+    public enum Player
+    {
+        None,
+        Red,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            switch(player)
+            {
+                case Player.Red: return Player.Black;
+                case Player.Black: return Player.Red;
+                default: return Player.None;
+            }
+        }
+
+    }
+}
