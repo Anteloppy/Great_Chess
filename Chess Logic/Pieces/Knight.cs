5477bbdd366af03cfa241f6a1c4e22fea84fd45d
Anteloppy <114159655+Anteloppy@users.noreply.github.com>
2024-04-30 23:13:16 +0700

    0.6

--- Chess Logic/Pieces/Knight.cs
+++ Chess Logic/Pieces/Knight.cs
@@ -1,4 +1,7 @@
-﻿namespace Chess_Logic
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chess_Logic
 {
     public class Knight : Piece
     {
@@ -15,6 +18,28 @@
             Knight copy = new Knight(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private static IEnumerable<Position> PotentialToPositions(Position from)
+        {
+            foreach (Direction vDir in new Direction[] { Direction.North, Direction.South })
+            {
+                foreach (Direction hDir in new Direction[] { Direction.East, Direction.West })
+                {
+                    yield return from + 2 * vDir + hDir;
+                    yield return from + 2 * hDir + vDir;
+                }
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from).Where(pos => Board.IsInside(pos) && (board.IsEmpty(pos) || board[pos].Color != Color));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositions(from, board).Select(to => new NormalMove(from, to));
         }
     }
 }
--- Chess Logic/Pieces/Pawn.cs
+++ Chess Logic/Pieces/Pawn.cs
@@ -1,13 +1,23 @@
-﻿namespace Chess_Logic
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chess_Logic
 {
     public class Pawn : Piece
     {
         public override PieceType Type => PieceType.Pawn;
         public override Player Color { get; }
 
+        private readonly Direction forward;
+
         public  Pawn(Player color)
         {
             Color = color;
+            switch (Color)
+            {
+                case Player.Red: forward = Direction.North; break;
+                case Player.Black: forward = Direction.South; break;
+            }
         }
 
         public override Piece Copy()
@@ -15,6 +25,49 @@
             Pawn copy = new Pawn(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private static bool CanMoveTo(Position pos, Board board)
+        {
+            return Board.IsInside(pos) && board.IsEmpty(pos);
+        }
+
+        private bool CanCaptureAt(Position pos, Board board)
+        {
+            if (!Board.IsInside(pos) || board.IsEmpty(pos))
+                return false;
+            return board[pos].Color != Color;
+        }
+
+        private IEnumerable<Move> ForwardMoves(Position from, Board board)
+        {
+            Position oneMovePos = from + forward;
+
+            if (CanMoveTo(oneMovePos, board))
+            {
+                yield return new NormalMove(from, oneMovePos);
+
+                Position twoMovesPos = oneMovePos + forward;
+
+                if (!HasMoved && CanMoveTo(twoMovesPos, board))
+                    yield return new NormalMove(from, twoMovesPos);
+            }
+        }
+
+        private IEnumerable<Move> DiagonalMoves(Position from, Board board)
+        {
+            foreach (Direction dir in new Direction[] { Direction.West, Direction.East })
+            {
+                Position to = from + forward + dir;
+
+                if (CanCaptureAt(to, board))
+                    yield return new NormalMove(from, to);
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return ForwardMoves(from, board).Concat(DiagonalMoves(from, board));
         }
     }
 }
