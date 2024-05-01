5a023a574538503408610777f025fc0a48d88ba1
Anteloppy <114159655+Anteloppy@users.noreply.github.com>
2024-05-01 16:48:03 +0700

    0.12

---
+++ Chess Logic/Moves/PawnPromotion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Logic
+{
+    public class PawnPromotion : Move
+    {
+        public override MoveType Type => MoveType.PawnPromotion;
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+
+        private readonly PieceType newType;
+
+        public PawnPromotion(Position from, Position to, PieceType newType)
+        {
+            FromPos = from;
+            ToPos = to;
+            this.newType = newType;
+        }
+
+        private Piece CreatePromotionPiece(Player color)
+        {
+            switch (newType)
+            {
+                case PieceType.Rook: return new Rook(color);
+                case PieceType.Knight: return new Knight(color);
+                case PieceType.Bishop: return new Bishop(color);
+                default: return new Queen(color);
+            }
+        }
+
+        public override void Execute(Board board)
+        {
+            Piece pawn = board[FromPos];
+            board[FromPos] = null;
+
+            Piece promotionPiec = CreatePromotionPiece(pawn.Color);
+            promotionPiec.HasMoved = true;
+            board[ToPos] = promotionPiec;
+        }
+    }
+}
---
+++ Chess UI/PromotionMenu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Chess_Logic;
+using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Chess_UI
+{
+    /// <summary>
+    /// Логика взаимодействия для PromotionMenu.xaml
+    /// </summary>
+    public partial class PromotionMenu : UserControl
+    {
+        public event Action<PieceType> PieceSelected;
+
+        public PromotionMenu(Player player)
+        {
+            InitializeComponent();
+
+            QueenImg.Source = Images.GetImage(player, PieceType.Queen);
+            BishopImg.Source = Images.GetImage(player, PieceType.Bishop);
+            KnightImg.Source = Images.GetImage(player, PieceType.Knight);
+            RookImg.Source = Images.GetImage(player, PieceType.Rook);
+        }
+
+        private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Queen);
+        }
+
+        private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Bishop);
+        }
+
+        private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Knight);
+        }
+
+        private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Rook);
+        }
+    }
+}
